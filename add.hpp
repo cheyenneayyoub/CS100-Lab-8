595702db860704febf61f36d285d0dae333504cb
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-24 22:59:48 -0700

    Implemented num of children and get child

---
+++ add.hpp
@@ -0,0 +1,42 @@
+#ifndef __ADD_HPP__
+#define __ADD_HPP__
+#include "base.hpp"
+
+class Add : public Base{
+
+	private:
+		Base* lhs;
+		Base* rhs;
+	public:
+		Add(Base* left, Base* right){
+			lhs = left;
+			rhs = right;
+		}
+		double evaluate() {
+			return lhs->evaluate() + rhs->evaluate();
+		}
+		string stringify(){
+			return "(" + lhs->stringify() + "+" + rhs->stringify() + ")";
+		}
+		int number_of_children(){
+			int num = 0;
+			if(lhs != NULL){
+				num += 1;
+			}
+			if(rhs != NULL){
+				num += 1;
+			}
+			return num;
+		}
+		Base* get_child(int i){
+			if(i == 0){
+				return lhs;
+			}
+			if(i == 1){
+				return rhs;
+			}	
+		}
+
+};
+
+#endif
