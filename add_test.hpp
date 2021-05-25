92d53b338d8e458716ff0153b9c9ac6af1e9369b
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-25 13:27:05 -0700

    working

--- add_test.hpp
+++ add_test.hpp
@@ -9,67 +9,82 @@
 #include "op.hpp"
 
 TEST(AddTest, AddEvalPositive){
-	Op* val1 = new Op(1.0);
-	Op* val2 = new Op(2.8);
-	Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+	val1 = new Op(1.0);
+	val2 = new Op(2.8);
+	Add *test = nullptr;
+	test = new Add(val1, val2);
 	EXPECT_EQ(test->evaluate(),3.8);
 	delete val1, val2, test;
 }
 
 TEST(AddTest, AddEvalZero){
-        Op* val1 = new Op(0.0);
-        Op* val2 = new Op(0.0);
-        Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+        val1 = new Op(0.0);
+        val2 = new Op(0.0);
+        Add *test = nullptr;
+	test = new Add(val1, val2);
         EXPECT_EQ(test->evaluate(),0.0);
 	delete val1, val2, test;
 }
 
 TEST(AddTest, AddEvalNegative){
-        Op* val1 = new Op(-1.0);
-        Op* val2 = new Op(-2.8);
-        Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+        val1 = new Op(-1.0);
+        val2 = new Op(-2.8);
+        Add *test = nullptr;
+	test = new Add(val1, val2);
         EXPECT_EQ(test->evaluate(),-3.8);
 	delete val1, val2, test;
 }
 
 
 TEST(AddTest, AddStringPos){
-	Op* val1 = new Op(1.0);
-	Op* val2 = new Op(2.8);
-	Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+	val1 = new Op(1.0);
+	val2 = new Op(2.8);
+	Add *test = nullptr;
+	test = new Add(val1, val2);
 	EXPECT_EQ(test->stringify(), "(1.000000+2.800000)");
 	delete val1, val2, test;
 }
 
 
 TEST(AddTest, AddStringNeg){
-        Op* val1 = new Op(-1.0);
-        Op* val2 = new Op(-2.8);
-        Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+        val1 = new Op(-1.0);
+        val2 = new Op(-2.8);
+        Add *test = nullptr;
+	test = new Add(val1, val2);
         EXPECT_EQ(test->stringify(), "(-1.000000+-2.800000)");
 	delete val1, val2, test;
 }
 
 TEST(AddTest, AddStringZero){
-        Op* val1 = new Op(0.0);
-        Op* val2 = new Op(0.0);
-        Add* test = new Add(val1, val2);
+	Op *val1 = nullptr, *val2 = nullptr;
+        val1 = new Op(0.0);
+        val2 = new Op(0.0);
+        Add *test = nullptr;
+	test = new Add(val1, val2);
         EXPECT_EQ(test->stringify(), "(0.000000+0.000000)");
 	delete val1, val2, test;
 }
 
 TEST(AddTest, NumOfChildren){
-	Base* val1 = new Op(3.0);
-	Base* val2 = new Op(2.0);
-	Base* addn = new Add(val1,val2);
+	Base *val1 = nullptr, *val2 = nullptr;	
+	val1 = new Op(3.0);
+	val2 = new Op(2.0);
+	Base* addn = nullptr;
+	addn = new Add(val1,val2);
 	EXPECT_EQ(addn->number_of_children(),2);
 	delete val1, val2, addn;
 }
 
 TEST(AddTest, getChild){
-	Base* val1 = new Op(3.0);
-        Base* val2 = new Op(2.0);
-	Base* addn = new Add(val1,val2);
+	Base *val1 = nullptr, *val2 = nullptr, *addn = nullptr;
+	val1 = new Op(3.0);
+        val2 = new Op(2.0);
+	addn = new Add(val1,val2);
 	EXPECT_EQ(addn->get_child(0),val1);
 	EXPECT_EQ(addn->get_child(1),val2);
 	delete val1, val2, addn;
