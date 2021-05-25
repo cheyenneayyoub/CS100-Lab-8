5e1df617a50fb72080e401c9a6a4c260ee5a1489
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-25 12:31:27 -0700

    fixed all tests

--- visitor_LaTeX_test.hpp
+++ visitor_LaTeX_test.hpp
@@ -20,7 +20,7 @@ TEST(VisitorLaTeXTest, OpCount) {
     Base* num[8] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7) };
 
     VisitorLaTeX* latex = new VisitorLaTeX();
-    string out = "${0}$\n";    
+    string out = "${0.000000}$\n";    
     EXPECT_EQ(latex->PrintLaTeX(num[0]), out);
 }
 
@@ -33,11 +33,11 @@ TEST(VisitorLaTeXTest, AllOperators) {
     sub   = new Sub(num[8], add);      
     mult  = new Mult(neg, sub);      
     div   = new Div(mult, num[4]);   
-    ans  = new Pow(div, num[4]);  ?? $(\\frac{-6}
+    ans  = new Pow(div, num[4]);  // $(\\frac{-6}
 
     VisitorLaTeX* latex = new VisitorLaTeX();
     string out = "$((\\frac{({-6}\\cdot({8}-({-6}+{-6})))}{{-4})^{4})$\n";
-    EXPECT_EQ(latex->PrintLaTeX(test), out);
+    EXPECT_EQ(latex->PrintLaTeX(ans), out);
 
     for (auto i : num) delete i;
     delete latex, ans;
@@ -56,20 +56,22 @@ TEST(VisitLaTeXTest, multiple1) {
         EXPECT_EQ(latex->PrintLaTeX(ans), out);
 
         for (auto i : num) delete i;
-    	    delete latex, a, b, test;
+    	    delete latex, left, right, ans;
 }
 
 TEST(VisitLaTeXTest, Multiple2) {
         Base* num[10] = {new Op(0), new Op(1), new Op(2), new Op(3), new Op(4), new Op(5), new Op(6), new Op(7), new Op(8), new Op(9) };
        
         Base* x = new Sub(num[8], num[0]); // 8-0= 8
-        Base* ans = new Add(num[0], num[x]); //0+(8-0)=8
-        Base* ans = new Sub(left , right);
+        Base* ans1 = new Add(num[0], x); //0+(8-0)=8
+        //Base* ans = new Sub(left , right);
            
         VisitorLaTeX* latex = new VisitorLaTeX();
         string out = "$({0}+{8}-{0}))$\n";
-        EXPECT_EQ(latex->PrintLaTeX(ans), out);
+        EXPECT_EQ(latex->PrintLaTeX(ans1), out);
     
         for (auto i : num) delete i;
-            delete latex, a, b, test;
+            delete latex, x, ans1;
 }
+
+#endif
